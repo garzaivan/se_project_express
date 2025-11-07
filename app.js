8e15681c8efd2f02dc17f575a84760e32cad86bb
ivangarza <garzaivan2709@yahoo.com>
2025-11-06 21:57:24 -0600

    Update Project 12

--- app.js
+++ app.js
@@ -5,10 +5,7 @@ const mainRouter = require("./routes/index");
 const app = express();
 const { PORT = 3001 } = process.env;
 
-mongoose
-  .connect("mongodb://127.0.0.1:27017/wtwr_db")
-  .then(() => {})
-  .catch((e) => console.error(e));
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 app.use(express.json());
 
